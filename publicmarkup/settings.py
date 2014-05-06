5a8f86cd12e47ef3207c25a37fa5d8f532fc6b97
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-05-06 17:18:52 -0400

    fix static_root

--- publicmarkup/settings.py
+++ publicmarkup/settings.py
@@ -72,7 +72,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static_root'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
